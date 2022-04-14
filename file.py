8ddf809cdf8bb2614aeaebd4b95a6221f94bff20
claudinei-casa <claudinei.casa@ufpr.br>
2022-04-14 15:18:01 -0300

    "Modified format from files, modified interface."

--- file.py
+++ file.py
@@ -1,53 +1,56 @@
 #!./venv/bin/python
 # -*- coding: utf-8 -*-
-import xlrd3
+import csv
 import os
-import sys
-from random import randint
+import shutil
 
 
-def help():
-    print("\n")
-    print(
-    ''' 
-	================================ Forma de uso ==========================
+def create_csv_file(id_profile, file, file_number):
+    '''
+    Create a csv file with id_profile = id for google
+    file = path to csv file with name
+    file_number = number of file when selected files > 1
+    '''
 
-	python relatorios-meis.py [id_profile] [nome_do_arquivo] [nome_do_arquivo]
+    # Size or numeber os lines - 1 (header) from csvs outputs
+    size_block = 999
+    data = []
+    # Read a csv file and save it in a list 
+    with open(file, "r") as f:
+        database = csv.reader(f)
+        for row in database:
+            data.append(row)
 
-	Ex:.
-	python relatorios-meis.py 409738522 teste1.xlsx teste2.xlsx teste3.xlsx ...
+    rows = data[1:]
+    header = data[:1]
+    number_of_rows = len(rows)
+    number_of_blocks = number_of_rows // size_block
 
-	========================================================================
-	'''
-    )
+    for item in range(1, number_of_blocks + 2):
+        print("Gerando arquivo(s): {}".format(str(file_number)))
 
+        # Column selected from the table, this column indicate the device IMEI on the input file csv.
+        column = 1
 
-def create_files(id_profile, files):
-    mkdir('relatorios')
-    for file in files:
-        workbook = xlrd3.open_workbook(file)
-        worksheet = workbook.sheet_by_index(0)
-        size_block = 999
-        for item in range(1, int(worksheet.nrows / size_block) + 2):
-            with open('./relatorios/relatorio-{}-imei-{}'.format(str(item),randint(0, 999999))+'.csv', "w", newline='\n') as f:
-                f.write("modemtype,modemid,manufacturer,profiletype,profileid\n")
-                if (worksheet.nrows / item) < size_block:
-                    for row in range(item*size_block-size_block, worksheet.nrows):
-                        f.write("IMEIS,{},Positivo,ZERO_TOUCH,{}\n".format(worksheet.cell_value(row, 2),id_profile,))
-                else:
-                    for row in range(item*size_block-size_block, item*size_block):
-                        f.write("IMEIS,{},Positivo,ZERO_TOUCH,{}\n".format(worksheet.cell_value(row, 2),id_profile,))
-                f.close()
-
-
-def clear():
-    if sys.platform == "linux":
-        os.system('clear')
-    else:
-        os.system('cls')
+        with open('./relatorios/relatorio-file-{}-imeis-item-{}.csv'.format(str(file_number),str(item)), "w", newline='\n') as f:
+            f.write("modemtype,modemid,manufacturer,profiletype,profileid\n")
+            # Final block contents or number_of_rows % size_block
+            if (number_of_rows / item) < size_block:
+                print("Planilha {}".format(str(item)))
+                for row in range(item*size_block-size_block, number_of_rows):                       
+                    f.write("IMEI,{},Positivo,ZERO_TOUCH,{}\n".format(rows[row][column],str(id_profile)))
+            # Another blocks
+            else:
+                print("Planilha {}".format(str(item)))
+                for row in range(item*size_block-size_block, item*size_block):
+                    f.write("IMEI,{},Positivo,ZERO_TOUCH,{}\n".format(rows[row][column],str(id_profile)))
 
 
 def mkdir(path):
+    #Delete the directory if it already exists
+    if file_exist(path):
+       remove_dir(path)
+
     try:
         os.mkdir(path)
     except OSError:
@@ -55,5 +58,13 @@ def mkdir(path):
     else:
         print ("Successfully created the directory %s " % path)
 
+
 def file_exist(path):
     return os.path.exists(path)
+
+
+def remove_dir(path):
+    try:
+        shutil.rmtree(path)
+    except OSError as e:
+        print(f"Error:{ e.strerror}")
--- main.py
+++ main.py
@@ -10,23 +10,32 @@ from file import *
 
 def select_files():
     filetypes = (
-        ('All files', '*.*'),
-        ('text files', '*.txt'),
-        ('xls files', '*.xl*'),
-        ('csv files', '*.csv'),
+        ('csv', '*.csv'),
     )
 
     filename = filedialog.askopenfilename(
         title='Open a files',
         initialdir='./',
         filetypes=filetypes)
-
-    showinfo(
-        title='Selected File',
-        message=filename
-    )
     
     return filename
+
+def create_files_splitted(id_profile, files):
+    mkdir('relatorios')   
+    try:
+        file_number = 0
+        for file in files:
+            create_csv_file(id_profile, file, file_number)
+            # To enumerate the files when selected files > 1
+            file_number += 1
+    except Exception as e:
+        print(e)
+        showinfo(
+            title="Error",
+            message="Error to create files. \n{}".format(e)
+        )
+    finally:
+        label_processes['text'] = '''Processo finalizado!'''
 
 
 def show_files_selected(list_files):
@@ -38,38 +47,114 @@ if __name__ == "__main__":
   
     root = Tk()
     root.title("Gerar Relatorios IMEIS")
+    root.configure(background="#333533")
+    label_color_background = "#333533"
+    label_color_fg = "#9381ff"
+    button_color_background = "#fff"
+    button_color_foreground = "#9381ff"
+    button_activebackground = "#9381ff"
 
-    # Button for select files to script
-    label_selector = Label(root, text="Selecione todas as planilhas que deseja gerar relatórios")
+    # Button to select files
+    label_selector = Label(
+        root,
+        text='''
+            Selecione todas as planilhas que deseja gerar relatórios no botão select file. 
+            ''',
+        background=label_color_background,
+        fg=label_color_fg,
+        font=("roboto", 14, "bold")
+    )
     label_selector.grid(row=0, column=3, padx=10, pady=10)
+
+    label_info = Label(
+        root,
+        text='''
+            OBS:\n  
+            1 - Somente arquivos .csv\n
+            2 - Após clicar em gerar sera criado uma pasta com nome relatórios que contem os arquivos de saida.\n
+            3 - Se já houver uma pasta com mesmo nome, esta sera apagada.\n
+            4 - Clique no botão Show Files se deejar visualizar os arquivos selecionados.
+            ''',
+        background=label_color_background,
+        fg=label_color_fg,
+        font=("roboto", 8)
+    )
+    label_info.grid(row=1, column=3)
+
     files = []
     open_button = Button(
         root,
         text='Select File',
-        command=lambda: files.append(select_files())
+        command=lambda: files.append(select_files()),
+        bg=button_color_background,
+        activebackground=button_activebackground,
+        fg=button_color_foreground,
+        border=0,
+        font=("roboto", 12, "bold"),
+        activeforeground=button_color_background,   
     )
-    open_button.grid(row=1, column=2, padx=10, pady=10)
+    open_button.grid(row=2, column=2, padx=10, pady=10)
 
-    # Files selected
-    label_files_selected = Label(root, text="")
-    label_files_selected.grid(row=2, column=3, padx=10, pady=10)
     show_button = Button(
         root,
         text='show files',
-        command=lambda: show_files_selected(files)
+        command=lambda: show_files_selected(files),
+        bg=button_color_background,
+        fg=button_color_foreground,
+        activebackground=button_activebackground,
+        border=0,
+        font=("roboto", 12, "bold"),
+        activeforeground=button_color_background
     )
-    show_button.grid(row=1, column=4, padx=10, pady=10)
+    show_button.grid(row=2, column=4, padx=10, pady=10)
+
+    # Files selected
+    label_files_selected = Label(
+        root,
+        text="", 
+        background=label_color_background, 
+        fg=label_color_fg,
+        font=("roboto", 10)
+    )
+    label_files_selected.grid(row=3, column=3, padx=10, pady=10)
 
     # Field with the id profile
-    label_id = Label(root, text="ID do Profile:")
-    label_id.grid(row=3, column=3, padx=10, pady=10)
+    label_id = Label(
+        root,
+        text="ID do Profile:",
+        background=label_color_background,
+        fg=label_color_fg,
+        font=("roboto", 12, "bold")
+    )
+    label_id.grid(row=4, column=3, padx=10, pady=10)
+
     id_profile = StringVar()
-    field_id_profile = Entry(root, textvariable=id_profile)
-    field_id_profile.grid(row=4, column=3, padx=10, pady=10)
+    field_id_profile = Entry(root, textvariable=id_profile, border=0)
+    field_id_profile.grid(row=5, column=3, padx=10, pady=10)
 
     # Button for creating the files
-    button_create_files = Button(root, text="Gerar Relatorios", command=lambda: create_files(id_profile.get(), files))
-    button_create_files.grid(row=5, column=3, padx=10, pady=10)
+    button_create_files = Button(
+        root, 
+        text="Gerar Relatorios",
+        command=lambda: create_files_splitted(id_profile.get(), files),
+        bg=button_color_background,
+        fg=button_color_foreground,
+        activebackground=button_activebackground,
+        border=0,
+        font=("roboto", 12, "bold"),
+        activeforeground=button_color_background
+    )
+    button_create_files.grid(row=6, column=3, padx=10, pady=10)
+
+    label_processes = Label(
+        root,
+        text="",
+        background=label_color_background,
+        fg=label_color_fg,
+        font=("roboto", 14, "bold")
+    )
+    label_processes.grid(row=7, column=3, padx=10, pady=10)
+
 
     root.mainloop()
 
